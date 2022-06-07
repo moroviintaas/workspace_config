5c459232d6fb47c5dbae48df1d716a6cabf08297
moroviintaas <moroviintaas@gmail.com>
2022-06-07 09:32:34 +0200

    My sway/alacritty config

---
+++ homedir/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+set -Ux _JAVA_AWT_WM_NONREPARENTING 1
+starship init fish | source
